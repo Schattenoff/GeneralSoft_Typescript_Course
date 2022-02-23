142dbd37569f60b9190cb742f1fe69c76747b346
Schatten WORK-PC <schattenwork@example.com>
2022-02-23 19:24:35 +0300

    task 2 (process)

--- Task 2/generics.ts
+++ Task 2/generics.ts
@@ -1,5 +1,66 @@
-import { CustomMap } from "./CustomMap"
-import { City, Country } from "./interfaces"
+class Pair <KeyType, ValueType> {
+    constructor(readonly key: KeyType, readonly value: ValueType) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class CustomMap <KeyType, ValueType> {
+    private array: Pair<KeyType, ValueType>[] = [];
+
+    toString() {
+       this.arr.forEach(item => {
+            `${item.key}->${item.value}`
+        })
+    }
+
+    add(key: KeyType, value: ValueType): void {
+        if(this.has(key)) { 
+            console.log(`You already have pair with key ${key}`);
+        } else {
+            this.arr.push(new Pair(key, value));
+        }
+    }
+    
+    remove(key: KeyType) {
+        if(this.has(key)){
+            this.arr.forEach((item, index) => {
+                if(item.key === key) {
+                    this.arr.splice(index, 1);
+                }
+            })
+        } else {
+            console.log(`Pair with key ${key} doesn't exist`);
+        }
+    }
+
+    get(key: KeyType) {
+        if(this.has(key)){
+            let [array] = this.arr.filter(item => item.key === key)
+            return array.value;
+        } else {
+            return `Pair with key ${key} doesn't exist`;
+        }
+    }
+
+    has(key: KeyType): boolean {
+        return this.arr.some(item => item.key === key);
+     }
+
+    get arr() : Pair<KeyType, ValueType>[] {
+        return this.array;
+    }
+}
+
+interface City {
+    title: string;
+    population: number;
+}
+
+interface Country {
+    capital: City;
+    totalArea: number;
+}
 
 const moscow: City = {
     title: 'Moscow',
