ea54b3c064447ca434a56c1bcc5cbc62931e48b3
Schatten WORK-PC <schattenwork@example.com>
2022-02-23 18:05:34 +0300

    task 1 (ready)

--- Task 1/store.ts
+++ Task 1/store.ts
@@ -1,5 +1,76 @@
-import { Product, Category } from './Product';
-import { User } from './User';
+enum Category {
+    phones = 'phones',
+    laptops = 'laptops',
+    watches = 'watches'
+}
+
+class Product {
+    private idProduct: number;
+
+    constructor(readonly price: number, readonly category: Category, readonly title: string) {
+        this.idProduct = Product.generationID();
+    }
+    private static generationID() : number {
+        return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    }
+
+    get id() {
+        return this.idProduct;
+    }
+}
+
+class User {
+    private basket : Product[] = [];
+
+    addToBasket(things : Product | Product[]) : void {
+        //Product Array
+        if (Array.isArray(things)) {
+            things.forEach((thing : Product) => {
+                this.addToBasketThing(thing);
+            })
+        } else {
+            //Product One
+            this.addToBasketThing(things);
+        }
+    }
+
+    addToBasketThing(thing : Product) {
+        if (this.checkID(thing)) {
+            console.log(`You already have ${thing.title} in your basket`);
+        } else {
+            this.basket.push(thing);
+        }
+    }
+
+    private checkID(thing : Product) : boolean {
+        return this.basket.some(element => element.id === thing.id);
+    } 
+
+    getProductsByCategory(category : Category) : Product[] {
+        return this.basket.filter(element => element.category === category);
+    }
+
+    getMostExpensiveProduct() {
+        return this.basket.reduce((previousValue, currentValue) => previousValue.price > currentValue.price ? previousValue : currentValue);
+    }
+
+    getMostInexpensiveProduct() {
+        return this.basket.reduce((previousValue, currentValue) => previousValue.price < currentValue.price ? previousValue : currentValue);
+    }
+
+    removeProduct(id : number) {
+        this.basket.splice(this.basket.findIndex(item => item.id === id), 1);
+    }
+
+    calculateTotalPrice() : number {
+        return this.basket.reduce((total : number, product : Product) => total + product.price, 0);
+    }
+
+    buy() : void {
+        this.basket = [];
+        console.log('Thanks for buying. Enjoy your shopping!');
+    }
+}
 
 const iPhone = new Product(1500, Category.phones, 'iPhone');
 const galaxyNote = new Product(500, Category.phones, 'Galaxy Note');
@@ -24,4 +95,3 @@ console.log(user.getProductsByCategory(Category.phones));
 console.log(user.getMostInexpensiveProduct().title);
 
 user.buy();
-
